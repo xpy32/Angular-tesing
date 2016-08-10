a6cdb5e53fd22c99a6ffe889d83c5b3fa4621c71
Guang Yang <yangguang@xionghaizhi.me>
2016-08-10 22:44:47 +0200

    first commit

---
+++ hero.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+  getHeroes() {
+    return Promise.resolve(HEROES);
+  }
+  getHeroesSlowly() {
+    return new Promise<Hero[]>(resolve =>
+      setTimeout(() => resolve(HEROES), 2000) // 2 seconds
+    );
+  }
+}
